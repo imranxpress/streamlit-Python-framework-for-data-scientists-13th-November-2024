e62c2ac0fa74240abfc34f3ad627e62b22f07598
sumonpaul18 <sumonpaul461@gmail.com>
2024-11-10 21:27:53 +0600

    upload two folder

---
+++ streamlit-text-display/text-display.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("Welcome to Paulco Cloud")
+st.header("Explore The Paulco Cloud")
+st.text("Hello")
+email = st.text_input("Email")
+feedback = st.text_area("Enter Your Feedback")
+button = st.button("Go")
+st.text_input("your")
+button = st.button("Going")
