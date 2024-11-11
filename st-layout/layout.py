e360088d51ab0e251c180c45d6a09a5150ce690c
sumonpaul18 <sumonpaul461@gmail.com>
2024-11-11 22:06:52 +0600

    add

--- st-layout/layout.py
+++ st-layout/layout.py
@@ -1,7 +1,15 @@
 import streamlit as st
+
+
 st.title("Create a Project")
 
 pname,ppass = st.columns(2)
 
 pname.text_input("Project Name")
 ppass.text_input("Password")
+
+col1,col2,col3 = st.columns(3)
+
+col1.text_input("A")
+col2.text_input("B")
+col3.text_input("C")
